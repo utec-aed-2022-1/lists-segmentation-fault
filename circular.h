cd2822d4d9e9ea3036f2b0bd9434867e9d946ccc
TheJoigoMan <75008901+TheJoigoMan@users.noreply.github.com>
2022-04-12 23:57:59 -0500

    Update circular.h

--- circular.h
+++ circular.h
@@ -8,13 +8,47 @@ template <typename T>
 class CircularList : public List<T> {  
     private:
         Node<T>* head;//sentinel
+        Node<T>* tail;//sentinel
+
         int nodes; 
     public:
-        CircularList() : List<T>() { }
+        CircularList() : List<T>() {
+          head = nullptr;
+          tail = nullptr;
+        }
 
         ~CircularList(){
            // TODO
-        }       
+        }
+
+
+        void push_front(T data){
+          Node<T>* nuevo = new Node<T>(data);
+          nuevo -> next = head;
+          head -> prev = nuevo;
+          head = nuevo;
+          head->prev = tail;
+          tail->next = head;
+        }
+
+        void push_back(T data){
+            Node<T>* nuevo = new Node<T>(data);
+        
+            if(head == nullptr){ //CASO VACIO
+                nuevo->next = nuevo;
+                nuevo->prev = nuevo;
+                head = nuevo;
+                tail = nuevo;
+            }
+        
+            tail = head->prev;
+            tail->next = nuevo;
+            nuevo->prev = tail;
+            nuevo->next = head;
+            head->prev = nuevo;
+            tail = nuevo;
+        }
+
 };
 
 #endif
